5deb30c132cb2e97440b9ca9b767f66fe9c7e030
Sang Le <sang.le@notabasement.com>
2021-03-22 15:31:10 +0700

    Adjust package file

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AWSAppSync",
-            targets: ["AWSAppSync"]),
+            targets: ["AWSCore", "AWSAppSync", "AppSyncRealTimeClient", "SQLite", "Starscream"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AWSAppSync",
-            dependencies: []),
-        .testTarget(
-            name: "AWSAppSyncTests",
-            dependencies: ["AWSAppSync"]),
+        .binaryTarget(name: "AWSCore", path: "Frameworks/AWSCore.xcframework"),
+        .binaryTarget(name: "AWSAppSync", path: "Frameworks/AWSAppSync.xcframework"),
+        .binaryTarget(name: "AppSyncRealTimeClient", path: "Frameworks/AppSyncRealTimeClient.xcframework"),
+        .binaryTarget(name: "SQLite", path: "Frameworks/SQLite.xcframework"),
+        .binaryTarget(name: "Starscream", path: "Frameworks/Starscream.xcframework"),
     ]
 )
